82202a58a13cb9def85303f1115385c00f071932
Deepanjan De <deep@deepcreations.net>
2020-12-29 14:56:02 -0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Deepanjan De on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
